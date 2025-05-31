f8ba6be02c9186c69aaffebe9a19755982de81ff
quantompop <friedmanjn@gmail.com>
2025-05-31 11:58:03 -0400

    Add global object polyfill for draft-js

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Polyfill global for draft-js
+// Polyfill for global object required by draft-js
 (window as any).global = window;
 
 createRoot(document.getElementById('root')!).render(
