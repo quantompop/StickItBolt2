2e3c31824e0884db49454c9867535d73fa745026
quantompop <friedmanjn@gmail.com>
2025-05-31 11:57:57 -0400

    Fix global is not defined error

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Polyfill global for draft-js
+(window as any).global = window;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
